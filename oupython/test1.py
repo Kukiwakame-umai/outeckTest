f23e2ed866e60c668d995d9d238dfa70c444d525
kukiwakame <79923713+Kukiwakame-umai@users.noreply.github.com>
2025-05-08 19:50:59 +0900

    python3

---
+++ oupython/test1.py
@@ -0,0 +1,26 @@
+'''
+T = 'Taro'
+S = ' is Student'
+x = T + S
+print(x)
+
+N = 5
+A = 1
+while N>=A:
+    if N%A == 1:
+        print(A)
+    A += 1
+
+A = 100
+B = 0
+print(A/B)
+'''
+
+A = [100, 'Taro', -0.1, True]
+print('A[2]:'+str(A[2]))
+
+mat = [[0, 1], [-1, 3]]
+print('mat[0]:'+str(mat[0]))
+mat[1][0] = 2
+print('mat[1][0]:'+str(mat[1][0]))
+print('mat[-1]:'+str(mat[-1]))
